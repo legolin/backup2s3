6536e44056c53521f058dbaa6b60178777406868
Aric Walker <aricw@aric-walkers-macbook.local>
2010-04-17 12:38:00 -0600

    generator work

--- generators/backup2s3/backup2s3_generator.rb
+++ generators/backup2s3/backup2s3_generator.rb
@@ -1,10 +1,13 @@
-class BackupGenerator < Rails::Generator::Base
+class Backup2s3Generator < Rails::Generator::Base
 
   def manifest
     record do |m|
       
-      m.directory "lib/tasks"      
-      m.file      "backup2s3.rake",        "lib/tasks/backup2s3.rake"
+      m.directory("lib/tasks")
+      m.file("backup2s3.rake", "lib/tasks/backup2s3.rake")
+
+      m.directory("config")
+      m.file("backup2s3.yml", "config/backup2s3.yml")
 
       puts message
     end
@@ -15,7 +18,13 @@ class BackupGenerator < Rails::Generator::Base
 
     You have successfully installed backup2s3!
 
-    # Add to your crontab or whatever
+    1. Modify your configuration file:
+
+      config/backup2s3.yml
+
+    2.
+
+    
     rake backup2s3:backup:create
     rake backup2s3:backup:delete
     rake backup2s3:backup:list
